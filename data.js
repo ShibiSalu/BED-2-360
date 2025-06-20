fef0e9aaedfdc9126f05ce5c2c6f55eaed48c7ea
ShibiSalu <shibisalu796@gmail.com>
2025-06-20 13:58:17 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dressing-area",
+      "name": "DRESSING AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.0024943635194745184,
+        "pitch": 0.02817962886112113,
+        "fov": 1.327546672418055
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.022548472018666388,
+          "pitch": 0.44875526090115514,
+          "rotation": 6.283185307179586,
+          "target": "0-dressing-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bedroom",
+      "name": "BEDROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.0008479192896100329,
+        "pitch": -0.01450399627542076,
+        "fov": 1.2057134264664933
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06655099566066092,
+          "pitch": 0.6981550664387939,
+          "rotation": 0,
+          "target": "1-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
